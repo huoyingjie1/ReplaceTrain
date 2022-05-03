51b9fa3fdcdba9eac55c36ffd9d18adf4186f55f
huoyingjie1 <1210539847@qq.com>
2022-05-03 10:49:18 +0800

    ReplaceTrain

---
+++ js/chunk-2d0e502c.43443f2f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e502c"],{9351:function(e,t,n){"use strict";n.r(t);var u={name:"home",data:function(){return{}},computed:{UserName:function(){return this.$store.state.user.token}},methods:{}},r=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[e._v("个人中心")])},o=[],s=n("2455"),c=!1,a=null,i=null,l=null,d=Object(s["a"])(u,r,o,c,a,i,l);t["default"]=d.exports}}]);
+//# sourceMappingURL=chunk-2d0e502c.43443f2f.js.map
